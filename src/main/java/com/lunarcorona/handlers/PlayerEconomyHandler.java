36eebc68e34bb09ee75b1a57fba562fb828d1a0e
TheSilentNights <yangsichen080320@outlook.com>
2023-12-24 12:30:14 +0800

    添加essentials

---
+++ src/main/java/com/lunarcorona/handlers/PlayerEconomyHandler.java
@@ -0,0 +1,10 @@
+package com.lunarcorona.handlers;
+
+import com.lunarcorona.EasyPlayerTools;
+import org.bukkit.command.CommandSender;
+
+public class PlayerEconomyHandler {
+    public static void showPlayerEconomyWithName(CommandSender sender,String name){
+
+    }
+}
