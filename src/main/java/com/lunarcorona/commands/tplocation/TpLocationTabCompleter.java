0bef3c65af2af90bd3b7fb498d9aa39db9c2f9c1
TheSilentNights <yangsichen080320@outlook.com>
2023-12-31 12:29:13 +0800

    初始化tplocation类

---
+++ src/main/java/com/lunarcorona/commands/tplocation/TpLocationTabCompleter.java
@@ -0,0 +1,16 @@
+package com.lunarcorona.commands.tplocation;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class TpLocationTabCompleter implements TabCompleter {
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        return null;
+    }
+}
